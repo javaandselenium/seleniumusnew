265f2d4687b75640e1aa1a3d3001ad9e0d38cb4f
QSP <QSP@QSP>
2021-06-08 07:32:50 +0530

    WebElements

---
+++ src/webelements/Test5.java
@@ -0,0 +1,29 @@
+package webelements;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+	
+		WebElement ele = driver.findElement(By.id("twotabsearchtextbox"));
+		System.out.println(ele.getAttribute("dir"));
+		
+		
+	      driver.close();
+
+
+	}
+
+}
